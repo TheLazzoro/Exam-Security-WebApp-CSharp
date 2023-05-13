de0cdb3a610edf5221f1c625a046e9654aa8bb04
TheLazzoro <43842641+TheLazzoro@users.noreply.github.com>
2023-05-13 15:33:54 +0200

    remove test code

--- WebApp/Controllers/WeatherForecastController.cs
+++ WebApp/Controllers/WeatherForecastController.cs
@@ -24,61 +24,6 @@ namespace WebApp.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            using (MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;uid=dev;pwd=ax2;database=exam-security"))
-            {
-                connection.Open();
-                MySqlCommand command = connection.CreateCommand();
-                MySqlTransaction transaction;
-
-                // Start a local transaction.
-                transaction = connection.BeginTransaction();
-
-                // Must assign both transaction object and connection
-                // to Command object for a pending local transaction
-                command.Connection = connection;
-                command.Transaction = transaction;
-
-                try
-                {
-                    // Prepared statement query
-                    command.CommandText = "Insert into db_user (username, passwd) VALUES (@username, @password)";
-                    MySqlParameter username = new MySqlParameter("@username", MySqlDbType.VarChar);
-                    MySqlParameter password = new MySqlParameter("@password", MySqlDbType.VarChar);
-                    username.Value = "user";
-                    password.Value = "pass";
-                    command.Parameters.Add(username);
-                    command.Parameters.Add(password);
-                    command.Prepare();
-                    command.ExecuteNonQuery();
-
-                    // Attempt to commit the transaction.
-                    transaction.Commit();
-                    Console.WriteLine($"Created user 'user'.");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
-                    Console.WriteLine("  Message: {0}", ex.Message);
-
-                    // Attempt to roll back the transaction.
-                    try
-                    {
-                        transaction.Rollback();
-                    }
-                    catch (Exception ex2)
-                    {
-                        // This catch block will handle any errors that may have occurred
-                        // on the server that would cause the rollback to fail, such as
-                        // a closed connection.
-                        Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
-                        Console.WriteLine("  Message: {0}", ex2.Message);
-                    }
-                }
-            }
-
-
-
-
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
